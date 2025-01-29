91967540a93a585de97c99f5a196b693455269ee
Mary Monchari Gichana <marygichana95@gmail.com>
2025-01-29 16:13:12 +0300

    Add Home component with Header and Contact components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,12 @@
 import React from "react";
 import Header from "./Header";
 import Contact from "./Contact";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+      <Contact />
+    </div>
+  );
+}
