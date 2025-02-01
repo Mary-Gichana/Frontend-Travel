31182a6f0b6ce860e583a5f0c782c3261ef51b91
Mary Monchari Gichana <marygichana95@gmail.com>
2025-02-01 22:55:29 +0300

    created and added style to  Trip component

--- src/components/Trip.jsx
+++ src/components/Trip.jsx
@@ -7,22 +7,17 @@ function Trip({ id, name, start_date, end_date, handleDeleteTrip }) {
 
   return (
     <div className="max-w-xs bg-white shadow-lg rounded-lg overflow-hidden transition duration-300 hover:shadow-xl transform hover:scale-105">
-      {/* Card Content */}
       <div className="p-4">
-        {/* Trip Name */}
         <h2 className="text-xl font-semibold text-gray-800 mb-2">{name}</h2>
 
-        {/* Trip Dates */}
         <p className="text-gray-600 text-sm mb-4">
           <strong>Start Date:</strong>{" "}
-          {new Date(start_date).toLocaleDateString()}
         </p>
         <p className="text-gray-600 text-sm mb-4">
-          <strong>End Date:</strong> {new Date(end_date).toLocaleDateString()}
+          <strong>End Date:</strong>
         </p>
       </div>
 
-      {/* Card Footer with Delete Button */}
       <div className="bg-gray-100 p-4 text-right">
         <button
           onClick={handleDelete}
